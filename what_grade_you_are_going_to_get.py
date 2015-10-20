6382fdbecf3d81923c207a012ae7b69d64dfc807
Shin Yamagami <kiboh.usa@gmail.com>
2015-10-19 23:03:04 -0700

    divide the function into small ones

--- what_grade_you_are_going_to_get.py
+++ what_grade_you_are_going_to_get.py
@@ -3,22 +3,29 @@
 # This program tell you what grade you are going to get.
 #
 
-def main():
-#Get the score of users.
-    point = int(input("What score did you get?: "))
-#Convert point to grade
-    point_to_grade(point)
+def get_score():
+    return int(input("What score did you get?: "))
 
 def point_to_grade(point):
     if point >= 800:
-        print('Your grade is A')
+        return 'A'
     elif point < 800 and point >= 700:
-        print('Your grade is B')
+        return 'B'
     elif point < 700 and point >= 600:
-        print('Your grade is C')
+        return 'C'
     elif point < 600 and point >= 500:
-        print('Your grade is D')
+        return 'D'
     else:
-        print('Your grade is F')
+        return 'F'
+    
+def print_grade(grade):
+    print('Your grade is', grade)
+
+def main():
+    point = get_score()
+    
+    grade = point_to_grade(point)
+
+    print_grade(grade)
 
 main()
